ff0b5c9cb297c4bf01ee9b90f5b5365d5d931866
Eric Tompkins <eric@aptuitiv.com>
2025-07-02 10:16:17 -0400

    Added format-theme-json command line option

--- src/theme.js
+++ src/theme.js
@@ -2,6 +2,15 @@
     Theme actions
 =========================================================================== */
 
+import chalk from 'chalk';
+import fancyLog from 'fancy-log';
+import fs from 'fs-extra';
+import logSymbols from 'log-symbols';
+
+// Lib
+import { isObject, isStringWithValue } from './lib/types.js';
+import { objectHasValue } from './lib/object.js';
+
 // Build files
 import config from './config.js';
 import {
@@ -69,4 +78,65 @@ export const themeHandler = async (action) => {
     }
 };
 
-export default themeHandler;
+/**
+ * Reorder the theme config object
+ *
+ * @param {object} obj The object to reorder
+ * @returns {object} The reordered object
+ */
+const reorderThemeConfig = (obj) => {
+    if (Array.isArray(obj)) {
+        return obj.map(reorderThemeConfig);
+    } if (isObject(obj)) {
+        // The data keys and the order that they should be in the object.
+        // Any other keys will be added to the end of the object in the order that they already are in the object.
+        const reorderKeys = ['name', 'label', 'type', 'description'];
+        const newObj = {};
+        // Pull out the keys that we want in a specific order
+        reorderKeys.forEach((key) => {
+            if (objectHasValue(obj, key)) {
+                newObj[key] = obj[key];
+            }
+        });
+        // Add the rest of the keys to the end of the object in the order that they already are in the object
+        for (const key of Object.keys(obj)) {
+            if (!reorderKeys.includes(key)) {
+                newObj[key] = reorderThemeConfig(obj[key]);
+            }
+        }
+        return newObj;
+    }
+    return obj;
+};
+
+/**
+ * Format a single theme setting file
+ *
+ * @param {string} [fileName] The name of the file to format.
+ */
+const formatThemeJsonFile = async (fileName) => {
+    const filePath = `${prefixSrcPath(config.data.themeConfig.src)}/${fileName}`;
+    if (fs.existsSync(filePath)) {
+        const json = await fs.readJson(filePath);
+        const formattedJson = reorderThemeConfig(json);
+        fs.writeJSONSync(filePath, formattedJson, { spaces: 4 });
+        fancyLog(logSymbols.success, chalk.green(`Updated the ${fileName} file`));
+    } else {
+        fancyLog(logSymbols.error, chalk.red(`The ${fileName} file does not exist`));
+    }
+};
+
+/**
+ * Format a single theme setting file or all theme setting files
+ *
+ * @param {string} [fileName] The name of the file to format. If not set then all theme setting files will be formatted.
+ */
+export const formatThemeJson = async (fileName) => {
+    if (isStringWithValue(fileName)) {
+        await formatThemeJsonFile(fileName);
+    } else {
+        await formatThemeJsonFile('theme-config.json');
+        await formatThemeJsonFile('theme-settings.json');
+        await formatThemeJsonFile('theme-styles.json');
+    }
+};
